107d5dd5aae3445611235eab64915205ae37677e
Abhishek Arora <abhishek.arora@monotype.com>
2021-04-24 18:53:14 +0530

    added computed properties for image and inStock

--- main.js
+++ main.js
@@ -2,23 +2,33 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 1, color: 'green'},
-                {id: 2, color: 'blue'},
+                {id: 1, color: 'green', quantity: 40, image: './assets/images/socks_green.jpg'},
+                {id: 2, color: 'blue', quantity: 0, image: './assets/images/socks_blue.jpg'},
             ],
             cart: 0,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
         };
     },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        }
+    }, 
     methods: {
         addToCart() {
             this.cart += 1;
         },
-        changeImage(color) {
-            if (color == 'green') this.image = './assets/images/socks_green.jpg';
-            else this.image = './assets/images/socks_blue.jpg'
+        changeVariant(index) {
+            this.selectedVariant = index;
         }
     }
 });
