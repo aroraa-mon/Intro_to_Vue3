8b0f62648fe663a65c8986ed9845d9c1457ce69e
Abhishek Arora <abhishek.arora@monotype.com>
2021-04-24 19:03:21 +0530

    added product display component and refactored code

---
+++ components/productDisplay.js
@@ -0,0 +1,64 @@
+app.component('product-display', {
+    template:
+    /* html */
+    `<div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img :class="{'out-of-stock-img': !inStock}" v-bind:src="image">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock"> In stock </p>
+                <p v-else> Out of Stock </p>
+
+                <ul>
+                    <li v-for="detail in details">{{detail}}</li>
+                </ul>
+                <div 
+                    v-for="(variant, index) in variants" 
+                    :key=variant.id 
+                    @mouseover="changeVariant(index)"
+                    :style = "{backgroundColor: variant.color}"
+                    class="color-circle"
+                ></div>
+                <button 
+                    class="button" 
+                    :class="{ disabledButton: !inStock }"
+                    :disabled="!inStock"
+                    v-on:click="addToCart"
+                >Add to Cart</button>
+            </div>
+        </div>
+    </div>`,
+    data() {
+        return {
+            product: 'Socks',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1, color: 'green', quantity: 40, image: './assets/images/socks_green.jpg'},
+                {id: 2, color: 'blue', quantity: 0, image: './assets/images/socks_blue.jpg'},
+            ],
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+        };
+    },
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        }
+    }, 
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        changeVariant(index) {
+            this.selectedVariant = index;
+        }
+    }
+})
